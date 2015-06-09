bfe9ca6497ef03ac14418820f97a6cbd474f760d
Duytan Hoang <duytanhoang@Duytans-MacBook-Pro.local>
2015-06-09 23:49:40 +0100

    First few unit tests

--- src/__tests__/test-parallax-scroll.js
+++ src/__tests__/test-parallax-scroll.js
@@ -1,19 +1,39 @@
 'use strict';
 
+var parallaxScrollHTML =    '<section class="parallax-container">' +
+                                '<img class="parallax-element" src="images/mini.jpg" alt="Mini Cooper" />' +
+                                '<div class="parallax-content">' +
+                                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+                                '</div>' +
+                            '</section>' +
+                            '<section class="parallax-container">' +
+                                '<img class="parallax-element" height="6000" src="images/amsterdam_clown.jpg" alt="Amsterdam art work" />' +
+                                '<div class="parallax-content">' +
+                                    'Pellentesque eu lorem blandit, vehicula eros eu, commodo justo. Nullam dui nibh, gravida a rhoncus eget, iaculis vitae elit. Pellentesque sit amet dui magna. Mauris blandit non odio a venenatis. Donec volutpat tellus ut iaculis sodales. Integer lacinia placerat purus, ac mollis elit interdum eu. Suspendisse blandit lacinia vehicula.' +
+                                '</div>' +
+                            '</section>';
+
 (function() {
-    describe('JS parallax scroll component', function() {
+    describe('Start JS parallax scroll component: ', function() {
+        var parallaxScroll;
 
         beforeEach(function(){
-            
+            document.body.innerHTML = parallaxScrollHTML;
+            parallaxScroll = new JSwidget.ParallaxScroll('selectorEl');
+            parallaxScroll.startParallaxScroll();
         });
 
         afterEach(function() {
-            
+            parallaxScroll = null;
         });
 
-        it('Populate window dimensions object', function() {
-            var parallaxScroll = new JSwidget.ParallaxScroll('selectorEl');
-            
+        it('Get ".parallax-container" elements', function() {
+            assert.strictEqual(parallaxScroll._parallaxContainerEl.length, 2);
         });
+        
+        // it('Set parallax element dimensions', function() {
+        //     assert.strictEqual(parallaxScroll._parallaxEl.style.cssText, 'width: 400px; height: auto; ');
+        // });
+        
     });
 })();
--- src/parallax-scroll.js
+++ src/parallax-scroll.js
@@ -8,19 +8,49 @@
 var JSwidget = JSwidget || {};
 
 (function() {
-	var ParallaxScroll = function(selector) {
+	var ParallaxScroll = function(selector, config) {
 		this._selector = selector;
-		this._winWidth = 0;
-		this._winHeight = 0;
+		this.config = config || {};
+		
+		// config options
+		this._scrollSpeed = this.config.scrollSpeed || 1;
+		
+		this._winWidth = window.innerWidth;
+		this._winHeight = window.innerHeight;
+		
+		this._parallaxContainerEl = document.getElementsByClassName('parallax-container');
+		this._parallaxEl = null;
 	};
 	
 	/**************************************
 	UTILITY METHODS
 	**************************************/
-	
+
 	/**************************************
 	PRIVATE METHODS
 	**************************************/
+	
+	// set parallax image dimensions
+	ParallaxScroll.prototype._setParallaxImgDimensions = function() {
+		for (var i=0, length = this._parallaxContainerEl.length; i < length; i++) {
+			var parallaxEl = this._parallaxContainerEl[i].getElementsByClassName('parallax-element')[0];
+			
+			// this._parallaxEl.style.width = this._winWidth + 'px';
+			// this._parallaxEl.style.height = 'auto';
+			
+			console.log(this._parallaxContainerEl[i].offsetHeight);
+			
+			console.log(parallaxEl);
+            if (parallaxEl.offsetHeight <= this._winHeight) {
+				parallaxEl.style.width = 'auto';
+				parallaxEl.style.height = this._winHeight + 'px';
+            } else {
+				parallaxEl.style.width = this._winWidth + 'px';
+				parallaxEl.style.height = 'auto';
+			}
+			console.log(parallaxEl.style.cssText);
+		}
+	};
 	
 	/**************************************
 	ON EVENT METHODS
@@ -35,11 +65,12 @@ var JSwidget = JSwidget || {};
 	**************************************/
 	
 	// start parallax scroll
-		// get window dimensions
-		// set parallax container dimensions
-		// set parallax image dimensions
-		// bind scroll event handler
+	ParallaxScroll.prototype.startParallaxScroll = function() {
+		this._setParallaxImgDimensions();	// set parallax image dimensions
 		
+		// bind scroll event handler
+	};
+	
 	// stop parallax scroll
 	
 	/**************************************
@@ -47,8 +78,3 @@ var JSwidget = JSwidget || {};
 	**************************************/
 	JSwidget.ParallaxScroll = ParallaxScroll;
 })();
-
-// (function() {
-// 	var parallaxScroll = new JSwidget.ParallaxScroll('selector');
-// 	console.log('parallaxScroll: ', parallaxScroll);
-// })();
